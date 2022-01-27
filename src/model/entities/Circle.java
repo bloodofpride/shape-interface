1fa35cc08ced878122d86865162c41875b77f3c9
bloodofpride <antonio_maxwell@hotmail.com>
2022-01-27 08:20:11 -0300

    solution with abstract class only

---
+++ src/model/entities/Circle.java
@@ -0,0 +1,25 @@
+package model.entities;
+
+import model.entities.enums.Color;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle(Color color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/model/entities/Rectangle.java
@@ -0,0 +1,35 @@
+package model.entities;
+
+import model.entities.enums.Color;
+
+public class Rectangle extends Shape {
+    private Double width;
+    private Double height;
+
+    public Rectangle(Color color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    @Override
+    public Double area() {
+        return height * width;
+    }
+}
